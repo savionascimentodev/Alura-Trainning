a8ac714e489913cd35090aaa0ddfd995380ff2fb
Savio Nascimento <savionascimentodev@gmail.com>
2022-02-20 23:15:20 -0400

    ✨ create vue simple app

---
+++ FormacaoVue/Vue-ConstruindoAplicacoesSPA/alurapic/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <ul></ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "AluraPic",
+      fotos: [
+        {
+          foto1: {
+            id: 1,
+            url: "http://vortexcultural.com.br/images/2021/05/jujutsu_capa.png",
+            titulo: "jujutsu"
+          }
+        },
+        {
+          foto2: {
+            id: 1,
+            url: "https://geeksaw.com.br/wp-content/uploads/2021/02/image2.png",
+            titulo: "onePiece"
+          }
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+img {
+  height: 260px;
+  width: 220px;
+}
+</style>

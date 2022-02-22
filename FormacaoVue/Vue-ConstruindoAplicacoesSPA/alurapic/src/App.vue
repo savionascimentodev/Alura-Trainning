891d480180bea15197e064d8571b6cdf2542784b
Savio Nascimento <savionascimentodev@gmail.com>
2022-02-21 23:25:33 -0400

    🚧 Creating view components and encapsulation

--- FormacaoVue/Vue-ConstruindoAplicacoesSPA/alurapic/src/App.vue
+++ FormacaoVue/Vue-ConstruindoAplicacoesSPA/alurapic/src/App.vue
@@ -14,23 +14,14 @@ export default {
   data() {
     return {
       title: "AluraPic",
-      fotos: [
-        {
-          foto1: {
-            id: 1,
-            url: "http://vortexcultural.com.br/images/2021/05/jujutsu_capa.png",
-            titulo: "jujutsu"
-          }
-        },
-        {
-          foto2: {
-            id: 1,
-            url: "https://geeksaw.com.br/wp-content/uploads/2021/02/image2.png",
-            titulo: "onePiece"
-          }
-        }
-      ]
+      fotos: []
     };
+  },
+  created() {
+    let promise = this.$http.get("http://localhost:3000/v1/fotos");
+    promise
+      .then(response => response.json())
+      .then(fotos => (this.fotos = fotos));
   }
 };
 </script>

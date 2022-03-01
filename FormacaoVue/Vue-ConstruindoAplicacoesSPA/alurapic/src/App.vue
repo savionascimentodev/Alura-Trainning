aa2d9833b4bafeec69f2a2d1356b11f11b8736ba
Savio Nascimento <savionascimentodev@gmail.com>
2022-03-01 14:10:43 -0400

    ✨ chapter six Single page application and route creation

--- FormacaoVue/Vue-ConstruindoAplicacoesSPA/alurapic/src/App.vue
+++ FormacaoVue/Vue-ConstruindoAplicacoesSPA/alurapic/src/App.vue
@@ -1,71 +1,40 @@
 <template>
   <div class="header">
-    <h1 class="title">{{ title }}</h1>
-    <input
-      v-model="filtro"
-      type="search"
-      class="filtro"
-      placeholder="Filtrar pelo titulo"
-    />
-    <ul class="list-fotos">
-      <li class="fotos-item" v-for="foto of fotosComFitro" :key="foto.id">
-        <Painel :titulo="foto.titulo">
-          <ImagemResponsiva :url="foto.url" :titulo="foto.titulo" />
-        </Painel>
-      </li>
-    </ul>
+    <Menu :routes="routes" />
+
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-import Painel from "./components/Painel.vue";
-import ImagemResponsiva from "./components/ImagemResponsiva.vue";
+import Menu from "./components/Menu.vue";
+import { routes } from "./routes";
 
 export default {
-  components: { Painel, ImagemResponsiva },
+  components: { Menu },
   data() {
     return {
-      title: "AluraPic",
-      fotos: [],
-      filtro: ""
+      routes
     };
-  },
-  created() {
-    let promise = this.$http.get("http://localhost:3000/v1/fotos");
-    promise
-      .then(response => response.json())
-      .then(fotos => (this.fotos = fotos));
-  },
-  computed: {
-    fotosComFitro() {
-      if (this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), "i");
-        return this.fotos.filter(foto => exp.test(foto.titulo));
-      } else {
-        return this.fotos;
-      }
-    }
   }
 };
 </script>
-
 <style>
 .header {
   font-family: Helvetica;
   width: 96%;
   margin: 0 auto;
 }
-.title {
-  text-align: center;
+
+.pagina-enter,
+.pagina-leave-active {
+  opacity: 0;
 }
-.list-fotos {
-  list-style: none;
-}
-.list-fotos .fotos-item {
-  display: inline-block;
-}
-.filtro {
-  display: block;
-  width: 100%;
+
+.pagina-enter-active,
+.pagina-leave-active {
+  transition: opacity 0.4s;
 }
 </style>
---
+++ FormacaoVue/Vue-ConstruindoAplicacoesSPA/alurapic/src/components/Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <h1 class="title">{{ title }}</h1>
+    <input
+      v-model="filtro"
+      type="search"
+      class="filtro"
+      placeholder="Filtrar pelo titulo"
+    />
+    <ul class="list-fotos">
+      <li class="fotos-item" v-for="foto of fotosComFitro" :key="foto.id">
+        <Painel :titulo="foto.titulo">
+          <ImagemResponsiva :url="foto.url" :titulo="foto.titulo" />
+        </Painel>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Painel from "./Painel.vue";
+import ImagemResponsiva from "./ImagemResponsiva.vue";
+
+export default {
+  components: { Painel, ImagemResponsiva },
+  data() {
+    return {
+      title: "AluraPic",
+      fotos: [],
+      filtro: ""
+    };
+  },
+  created() {
+    let promise = this.$http.get("http://localhost:3000/v1/fotos");
+    promise
+      .then(response => response.json())
+      .then(fotos => (this.fotos = fotos));
+  },
+  computed: {
+    fotosComFitro() {
+      if (this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), "i");
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.title {
+  text-align: center;
+}
+.list-fotos {
+  list-style: none;
+}
+.list-fotos .fotos-item {
+  display: inline-block;
+}
+.filtro {
+  display: block;
+  width: 100%;
+}
+</style>
